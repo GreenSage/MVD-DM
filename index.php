71024a06ee02ed2c9a78cbb143963dafdf45212d
GreenSage <GreenSage39@gmail.com>
2016-07-24 13:39:23 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?
+$_SESSION['in'] = 0;
+$title = "Welcome!";
+require_once 'mvd.php';
+require_once 'metaHeader.php';
+?>
+
+<div class="welcome">
+  Hello and Welcome!
+</div>
+require_once 'metaFooter.php';
